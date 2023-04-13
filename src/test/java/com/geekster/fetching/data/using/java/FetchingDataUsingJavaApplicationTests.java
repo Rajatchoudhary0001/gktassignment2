d7d728e31d7608aa102d01d8f2a37a280bd5dc66
Rajat <rajatchoudhary9484@gmail.com>
2023-04-13 23:25:23 +0530

    first commit

---
+++ src/test/java/com/geekster/fetching/data/using/java/FetchingDataUsingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.fetching.data.using.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchingDataUsingJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
